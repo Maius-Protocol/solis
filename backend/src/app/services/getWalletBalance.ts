ab435fd688bcf9676747152d60f5d14ac25129d5
Tuan Pham Minh <tuanpmhd@gmail.com>
2023-08-04 15:49:20 +0700

    minor fix

--- backend/src/app/services/getWalletBalance.ts
+++ backend/src/app/services/getWalletBalance.ts
@@ -1,12 +1,13 @@
 import shyft from "@/app/adapters/shyft";
-import jupyter from "@/app/adapters/jupyter";
+import jupiter from "@/app/adapters/jupiter";
 import { SOL_ADDRESS, SOL_DECIMALS, SOL_LOGO } from "../../../env";
 
 export async function getWalletBalance(walletAddress: string) {
   const walletBalance = [];
   const solBalance = await shyft.getSolBalance(walletAddress);
-  const solData = await jupyter.getTokenPrice(SOL_ADDRESS);
+  const solData = await jupiter.getTokenPrice(SOL_ADDRESS);
   const solPrice = Object.keys(solData).map((key) => solData[key]);
+  console.log("solPrice:", solPrice)
 
   walletBalance.push({
     address: SOL_ADDRESS,
@@ -18,17 +19,18 @@ export async function getWalletBalance(walletAddress: string) {
   });
 
   const tokenBalances = await shyft.getTokenBalance(walletAddress);
+  console.log("tokenBalances:", tokenBalances)
   await Promise.all(
     tokenBalances.map(async (t: any) => {
       if (t.balance > 0) {
-        const data = await jupyter.getTokenPrice(t.address);
+        const data = await jupiter.getTokenPrice(t.address);
         const tokenPrice = Object.keys(data).map((key) => data[key]);
         walletBalance.push({
           address: t.address,
           balance: t.balance,
           symbol: t.info?.symbol,
           decimals: t.info?.decimals,
-          price: tokenPrice[0].price,
+          price: tokenPrice[0]?.price || 0,
           image: t.info?.image,
         });
       }
