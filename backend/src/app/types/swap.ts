292e68f99ea87d92b24313c21190a56bd602c116
Tuan Pham Minh <tuanpmhd@gmail.com>
2023-08-08 18:02:59 +0700

    add manual get combination swap routes logic

--- backend/src/app/types/swap.ts
+++ backend/src/app/types/swap.ts
@@ -1,16 +1,24 @@
+import { TokenBalance } from "./token"
+
 export enum CombinationSwapMode {
     AUTO = "auto",
     MANUAL = "manual"
+}
+
+export interface ManualSwapTokenInput {
+    inputToken: TokenBalance,
+    amount: number
 }
 
 export interface CombinationSwapRouteInput {
     walletAddress: string,
     outputAmount: number,
     outputMint: string,
+    manualSwapTokenInputList: ManualSwapTokenInput[]
     mode: CombinationSwapMode
 }
 
-export  interface CombinationSwapRoute {
+export interface CombinationSwapRoute {
     isSwapRoute: boolean,
     inputToken: {
         tokenInfo: any,
