8b7b896beec2035e90a21d08d66c28133ff8010b
Tuan Pham Minh <tuanpmhd@gmail.com>
2023-08-19 00:25:48 +0700

    minor fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useFonts } from "@expo-google-fonts/dev";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import NavigationScreen from "./screens/NavigationScreen/NavigationScreen";
 import { ConfigProvider } from "antd";
 import lottie from "lottie-web";
@@ -46,7 +46,7 @@ function App() {
       >
         <QueryClientProvider client={queryClient}>
           <NavigationScreen />
-          <ReactQueryDevtools initialIsOpen={false} />
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </QueryClientProvider>
       </ConfigProvider>
     </RecoilRoot>
